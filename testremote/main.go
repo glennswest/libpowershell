f7c39c78424f85c3ab438e28bcd79fc6fdd613de
glennswest <gwest@redhat.com>
2019-03-06 14:13:03 -0600

    Prepare Test Case

--- testremote/main.go
+++ testremote/main.go
@@ -2,8 +2,7 @@ package main;
 
 import(
 	    "fmt"
-            "strings"
-            "github.com/libpowershell/libwmi"
+            "github.com/glennswest/libpowershell/libwmi"
 	)
 
 
@@ -11,8 +10,8 @@ func main(){
   host := "winnode01"
   username := "Adminisrator"
   password := "Secret2018"
-  thever := WmiGetWinVersion(host,username,password);
-  fmt.Printf("Windows Version = %s\n",thever);
+  thever := libwmi.WmiGetWinVersion(host,username,password)
+  fmt.Printf("Windows Version = %s\n",thever)
 }
 
 

f1ae13dfac0e8294ae4ce52943f832ba1a11073b
glennswest <gwest@redhat.com>
2019-05-15 17:13:52 -0500

    Change to test powershell remote

--- testremote/main.go
+++ testremote/main.go
@@ -2,24 +2,21 @@ package main;
 
 import(
 	    "fmt"
-            "strings"
             "github.com/glennswest/libpowershell/pshell"
 	)
 
 
 func main(){
-  host := "winnode01"
+  host := "10.19.114.61"
   pshell.SetRemoteMode(host,"Administrator","Secret2018")
   
   thever := pshell.GetWinVersion();
   fmt.Printf("Windows Version = %s\n",thever)
  
-  result := pshell.Powershell("Test-Path -Path \"/Program` Files/WindowsNodeManager/winnodeman.exe\"")
+  pcmd := `$Env:master="openshift.green.k.e2e.bos.redhat.com";$Env:wmmurl="winmachineman.apps.green.k.e2e.bos.redhat.com";$Env:template="/templates/win10.0.17763.template";$Env:beta_kubernetes_io_os="windows";$Env:kubernetes_io_hostname="winnode01";$Env:beta_kubernetes_io_arch="amd64";$Env:host_ip="10.19.114.61";$Env:ovn_host_subnet="10.128.64.0/24";Set-ExecutionPolicy unrestricted;\bin\prereq1809.ps1`
+  result := pshell.Powershell(pcmd)
   fmt.Printf("Result = %s\n",result)
   fmt.Printf("Length = %d\n",len(result))
-  if (strings.Compare(result[0:4],"True") == 0){
-     fmt.Printf("It Really True\n")
-     }
 }
 
 
